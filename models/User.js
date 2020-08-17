523f3c0a56872ec87105bdb65acb4997e3a65dfc
crypt0 <acvjetan@gmail.com>
2020-08-17 10:33:55 +0200

    Add mongoose hooks.

--- models/User.js
+++ models/User.js
@@ -16,6 +16,18 @@ const userSchema = new mongoose.Schema({
   },
 });
 
+// fire a function after doc saved to db
+userSchema.post("save", function (doc, next) {
+  console.log("new user was created & saved", doc);
+  next();
+});
+
+// fire a function before doc saved to db
+userSchema.pre("save", function (next) {
+  console.log("user about to be created & saved", this);
+  next();
+});
+
 const User = mongoose.model("user", userSchema);
 
 module.exports = User;
